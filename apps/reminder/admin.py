d8d38849a9f1c44797804ed32a315541868bcda2
lester <lestermardy.gallardo@cit.edu>
2025-09-26 23:23:35 +0800

    registered the reminder app models in reminder app ADMIN.PY

--- apps/reminder/admin.py
+++ apps/reminder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from apps.reminder.models import Reminder, ReminderResponse
+
 # Register your models here.
+admin.register(Reminder)
+admin.register(ReminderResponse)
