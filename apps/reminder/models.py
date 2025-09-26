cb9a6aeb7aed1c28f2ba7dcf0c8aa72efeb98bf8
lester <lestermardy.gallardo@cit.edu>
2025-09-26 23:19:32 +0800

    added foreign key from the User model and initialize migrations

--- apps/reminder/models.py
+++ apps/reminder/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from ..user.models import User
 # Create your models here.
 
 class Reminder(models.Model):
     reminder_id = models.AutoField(primary_key=True)
     message = models.CharField(max_length=150, null=False)
     date_time = models.DateTimeField(null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Reminder #{self.reminder_id}"
@@ -14,7 +16,8 @@ class ReminderResponse(models.Model):
     response_id = models.AutoField(primary_key=True)
     status = models.CharField(max_length=150, null=False)
     date_responded = models.DateField(null=False)
-    reminder = models.ForeignKey(Reminder, on_delete=models.CASCADE)
-    
+    reminder = models.OneToOneField(Reminder, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"Response #{self.response_id}"
