96e1c9f2be46ed8fa9da31ffb57bda986e3bf344
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-09-26 23:50:32 +0800

    renamed fields to follow snake_case

--- apps/savings/models.py
+++ apps/savings/models.py
@@ -6,18 +6,18 @@ from apps.user.models import User
 # Create your models here.
 
 class SavingGoal(models.Model):
-    savinggoalid = models.AutoField(primary_key=True)
+    saving_goal_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    targetamount = models.DecimalField(decimal_places=2, max_digits=12)
-    targetdate = models.DateField()
+    target_amount = models.DecimalField(decimal_places=2, max_digits=12)
+    target_date = models.DateField()
 
     def __str__(self):
-        return f"{self.name} - Target: {self.targetdate}"
+        return f"{self.name} - Target: {self.target_date}"
 
 
 class Saving(models.Model):
-    savingid = models.AutoField(primary_key=True)
+    saving_id = models.AutoField(primary_key=True)
     goal = models.ForeignKey(SavingGoal, on_delete=models.CASCADE)
     amount = models.DecimalField(decimal_places=2, max_digits=12)
     date = models.DateField(auto_now_add=True)
