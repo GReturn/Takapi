cdb715e954daad8c2e9e81b0b2878b81d8cc7c4a
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-09-26 22:11:12 +0800

    added foreign key field access from the Currency model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from apps.currency.models import Currency
+
+
 # Create your models here.
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
@@ -9,6 +12,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=128)
     gender = models.CharField(max_length=11)
     age = models.IntegerField(default=0)
+    currency = models.ForeignKey(Currency, on_delete=models.RESTRICT)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
