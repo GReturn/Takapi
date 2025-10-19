90644b902485c9aee27d8d527c8669145ff9cc5c
lester <lestermardy.gallardo@cit.edu>
2025-10-19 14:20:51 +0800

    REMOVED import reminder response model in admin.py

--- apps/reminder/admin.py
+++ apps/reminder/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Reminder, ReminderResponse
+from .models import Reminder
 
 # Register your models here.
 admin.site.register(Reminder)
-admin.site.register(ReminderResponse)
