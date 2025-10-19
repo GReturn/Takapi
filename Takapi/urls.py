142ebf10a8517056dac382dc1f8e4c831336f327
martianK3jC <kceniza24@gmail.com>
2025-10-19 14:45:21 +0800

    Added an Expense page dashboard, no functioning features yet.

--- Takapi/urls.py
+++ Takapi/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from apps.expense import views as expense_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
