d957150c3084a9e1f337824f7428c630afa6b209
lester <lestermardy.gallardo@cit.edu>
2025-10-19 14:22:52 +0800

    ADDED view class for reminder view page and create reminder page

--- apps/reminder/views.py
+++ apps/reminder/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+class ReminderViewPage(View):
+    template_name = "view-reminder.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class CreateReminderPage(View):
+    template_name = "create-reminder.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
