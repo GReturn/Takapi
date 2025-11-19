f72fe4b5817f49b321c7cc0e608d8237be0d3815
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-11-19 10:48:00 +0800

    Added urls of signup, terms, and privacy

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('terms/', views.TermsView.as_view(), name='terms'),
+    path('privacy/', views.PrivacyView.as_view(), name='privacy'),
 ]
