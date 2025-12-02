c2c4f5ccc53ebf4b5762008261bc918e57a5b2c8
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-12-02 21:54:48 +0800

    changed model CharField max_length for Currency.currency_long_name to 128 and User.gender to 32

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Currency(models.Model):
     currency_id = models.AutoField(primary_key=True)
     currency_short_name = models.CharField(max_length=11)
-    currency_long_name = models.CharField(max_length=50)
+    currency_long_name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.currency_short_name + " " + self.currency_long_name
@@ -14,7 +14,7 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
-    gender = models.CharField(max_length=11)
+    gender = models.CharField(max_length=32)
     age = models.IntegerField(default=0)
     currency = models.ForeignKey(Currency, on_delete=models.RESTRICT)
 
