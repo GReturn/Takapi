d41c5124b1eae84919a68c5f7980b92a25eae6dc
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-12-03 03:09:12 +0800

    added app_name namespace for reminder

--- apps/reminder/urls.py
+++ apps/reminder/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'reminder'
+
 urlpatterns = [
-    path('', views.ReminderViewPage.as_view(), name="view"),
+    path('', views.ReminderViewPage.as_view(), name="index"),
     path('create', views.CreateReminderPage.as_view(), name="create"),
     path('edit', views.EditReminderPage.as_view(), name="edit"),
 ]
