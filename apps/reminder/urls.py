4bfc20660d3953b01faa326f9ff7756eb15f9b98
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-12-03 03:37:59 +0800

    reminder UI

--- apps/reminder/urls.py
+++ apps/reminder/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'reminder'
 
 urlpatterns = [
-    path('', views.ReminderViewPage.as_view(), name="index"),
-    path('create', views.CreateReminderPage.as_view(), name="create"),
-    path('edit', views.EditReminderPage.as_view(), name="edit"),
+    path('', views.ReminderIndexView.as_view(), name="index"),
+    path('create', views.CreateReminderView.as_view(), name="create"),
 ]
--- apps/reminder/views.py
+++ apps/reminder/views.py
@@ -1,25 +1,41 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
+from apps.reminder.models import Reminder
 
-# Create your views here.
-class ReminderViewPage(View):
-    template_name = "view-reminder-tw.html"
+# class ReminderViewPage(View):
+#     template_name = "view-reminder-tw.html"
+#
+#     def get(self, request):
+#         return render(request, self.template_name)
+#
+# class CreateReminderPage(View):
+#     template_name = "create-reminder-tw.html"
+#
+#     def get(self, request):
+#         return render(request, self.template_name)
+#
+# class EditReminderPage(View):
+#     template_name = "edit-reminder-tw.html"
+#
+#     def get(self, request):
+#         return render(request, self.template_name)
 
+
+class ReminderIndexView(View):
     def get(self, request):
-        return render(request, self.template_name)
+        # Fetch reminders for the logged-in user
+        user_id = request.session.get('user_id')
+        reminders = Reminder.objects.filter(user_id=user_id).order_by('date_time')
 
-class CreateReminderPage(View):
-    template_name = "create-reminder-tw.html"
+        return render(request, 'index.html', {'reminders': reminders})
 
-    def get(self, request):
-        return render(request, self.template_name)
 
-class EditReminderPage(View):
-    template_name = "edit-reminder-tw.html"
+class CreateReminderView(View):
+    def post(self, request):
+        # Logic to create reminder from POST data
+        # Combine date + time into datetime
+        # Save to DB
+        return redirect('reminder:index')
 
-    def get(self, request):
-        return render(request, self.template_name)
+# ... Edit and Delete views similarly ...
