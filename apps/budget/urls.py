b7446ad7ef63eb7c94f4bb6c8b5eb1b88b1cd9fa
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-12-03 03:08:55 +0800

    added app_name namespace for budget

--- apps/budget/urls.py
+++ apps/budget/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'budget'
+
 urlpatterns = [
-    path('', views.index, name='budget_index'),
+    path('', views.index, name='index'),
     path('create/', views.create_budget, name='create_budget'),
 ]
