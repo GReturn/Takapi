f9a0a689537ca4a8dcefad8152b71dd28936fb9d
lester <lestermardy.gallardo@cit.edu>
2025-12-17 06:53:05 +0800

    ADDED url for delete reminder

--- apps/reminder/urls.py
+++ apps/reminder/urls.py
@@ -6,4 +6,5 @@ app_name = 'reminder'
 urlpatterns = [
     path('', views.ReminderIndexView.as_view(), name="index"),
     path('create', views.CreateReminderView.as_view(), name="create"),
+    path('delete/<int:reminder_id>', views.DeleteReminderView.as_view(), name='delete'),
 ]
