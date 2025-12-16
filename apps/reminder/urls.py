7e224e62ae377c722d07816fde7139043d62515e
lester <lestermardy.gallardo@cit.edu>
2025-12-17 07:16:09 +0800

    ADDED url for edit reminder

--- apps/reminder/urls.py
+++ apps/reminder/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.ReminderIndexView.as_view(), name="index"),
     path('create', views.CreateReminderView.as_view(), name="create"),
     path('delete/<int:reminder_id>', views.DeleteReminderView.as_view(), name='delete'),
+    path('edit/<int:reminder_id>', views.EditReminderView.as_view(), name='edit'),
 ]
