fc156a23988380fa46acc28357ea5139605e7437
Acrux73 <jeesandeecaralde@gmail.com>
2025-12-17 23:13:07 +0800

    Implemented saving, saving goal

--- apps/savings/urls.py
+++ apps/savings/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('add-goal/', views.add_goal, name='add_goal'),
     path('add-saving/', views.add_saving, name='add_saving'),
     path('saving-history/', views.saving_history, name='saving_history'),
+    path('delete-goal/', views.delete_goal, name='delete_goal'),
+    path('edit-goal/', views.edit_goal, name='edit_goal'),
+    path('edit-saving/', views.edit_saving, name='edit_saving'),
+    path('delete-saving/', views.delete_saving, name='delete_saving'),
 ]
